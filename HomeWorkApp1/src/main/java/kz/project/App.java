17047beb621688eb13568827e18d24f292e2837b
bakdaulet <bakdauletcoutinho@gmail.com>
2024-04-09 23:19:26 +0500

    kefteme

---
+++ HomeWorkApp1/src/main/java/kz/project/App.java
@@ -0,0 +1,13 @@
+package kz.project;
+
+/**
+ * Homework App #01!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
