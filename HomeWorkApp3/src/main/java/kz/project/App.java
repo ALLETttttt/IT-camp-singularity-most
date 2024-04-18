25b6b36247c43c0b2dd8190463a9a45743df2ce9
bakdaulet <bakdauletcoutinho@gmail.com>
2024-04-19 00:41:59 +0500

    push

--- HomeWorkApp3/src/main/java/kz/project/App.java
+++ HomeWorkApp3/src/main/java/kz/project/App.java
@@ -1,5 +1,9 @@
 package kz.project;
 
+import java.sql.SQLOutput;
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,41 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        guessNumber();
+    }
+
+
+    public static void guessNumber() {
+        Random random = new Random();
+        Scanner input = new Scanner(System.in);
+
+
+        int attempts = 3;
+
+        do {
+            int randomNumber = random.nextInt(1,10);
+            boolean target = false;
+            System.out.println("System guessed number between 1 and 9: ");
+            for (int i = 0; i < attempts; i++) {
+                System.out.println("Enter your number: ");
+                int inputNumber = input.nextInt();
+
+                if (inputNumber == randomNumber) {
+                    target = true;
+                    System.out.println("Congratz!");
+                }
+                else if (inputNumber < randomNumber) {
+                    System.out.println("Entered number is smaller than guessed");
+                }
+                else {
+                    System.out.println("Entered number is bigger than guessed");
+                }
+            }
+            if (!target) {
+                System.out.println("You are lost, attempts is reached limit! Guessed number was: " + randomNumber);
+            }
+            System.out.println("Restart the game? 1 --> Yes  0 --> No");
+        } while (input.nextInt() == 1);
+        System.out.println("Bye!");
     }
 }
